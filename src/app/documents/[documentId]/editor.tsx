c2ee4b4af043e956f38e5fb514bcfc2d5c1084eb
SuperSection <soumosarkar297@gmail.com>
2024-12-21 11:36:12 +0530

    feature: add margin ruler component

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -18,14 +18,17 @@ import Highlight from "@tiptap/extension-highlight";
 import { Color } from "@tiptap/extension-color";
 import Link from "@tiptap/extension-link";
 
+import { INITIAL_MARGIN, PAGE_WIDTH } from "@/constants/document.const";
 import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extensions/font-size";
 import { LineHeightExtension } from "@/extensions/line-height";
+import { Ruler } from "./ruler";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -52,9 +55,9 @@ export const Editor = () => {
     },
     editorProps: {
       attributes: {
-        style: "padding-left: 56px; padding-right: 56px; ",
+        style: `padding-left: ${INITIAL_MARGIN}px; padding-right: ${INITIAL_MARGIN}px; width: ${PAGE_WIDTH}px;`,
         class:
-          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
     extensions: [
@@ -94,7 +97,11 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
-      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+      <Ruler />
+      <div
+        className="min-w-max flex justify-center py-4 print:py-0 mx-auto print:w-full print:min-w-0"
+        style={{ width: PAGE_WIDTH }}
+      >
         <EditorContent editor={editor} className="print:block" />
       </div>
     </div>
---
+++ src/constants/document.const.ts
@@ -0,0 +1,5 @@
+export const PAGE_WIDTH = 816;
+
+export const INITIAL_MARGIN = 56;
+
+export const MINIMUM_MARGIN = 100;
